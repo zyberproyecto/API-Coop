d79476174c5800d88b6f005c0ba365d1d4413e18
elpibearena <alejandrobarcelo822016@gmail.com>
2025-08-30 20:28:39 -0300

    Vaciar rama main

--- routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\HorasTrabajadasController;
-use App\Http\Controllers\Api\ComprobantePagoController;
-
-Route::post('/horas-trabajadas', [HorasTrabajadasController::class, 'store']);
-Route::post('/comprobantes-pago', [ComprobantePagoController::class, 'store']);
